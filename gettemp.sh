ad73d7c2301eb43a78f76c1390db839269cf58a0
Urfin78 <urfin78@gmail.com>
2015-08-01 12:24:59 +0200

    gettemp modified sqlite

--- gettemp.sh
+++ gettemp.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-# initialize variables
+DB="/root/test.db"
 TIME=`date "+%s"`
 SUMH=0
 SUMT=0
-# as sensor reads seem to fluctuate do the whole thing 10 times and take the average
+#read data from sensor until real values are read
 for ((i=1; i<=10; i++)); do
-# read data from sensor until real values are read
 	DATA=`~/dht11`
 	while [ "${DATA}" = "Invalid Data!!" ]; do
 		DATA=`~/dht11`
 		TIME=`date "+%s"`
 	done
-# capture values in array 0=hum,1=temp in celsius
-#
+#capture values in array 0=hum,1=temp in celsius
 	VALUES=($(echo "${DATA}"|grep -oe "[[:digit:]]*\.[[:digit:]]"))
 	HUM=($(echo "${VALUES[0]}"|grep -oP '.*?(?=\.)'))
 	TEMP=($(echo "${VALUES[1]}"|grep -oP '.*?(?=\.)'))
+#	echo ${VALUES[0]}
+#	echo ${VALUES[1]}
 	SUMH=$(( $SUMH + $HUM ))
 	SUMT=$(( $SUMT + $TEMP ))
 done
-#sqlite3 ${DB} "insert into data (sensor,time,hum,temp) VALUES (1,${TIME},${HUM},${TEMP});"
-# take the average
+#echo ${VALUES[0]}
+#echo ${VALUES[1]}
 HUMID=`echo "scale=1;$SUMH/10"|bc`
 TEMPERA=`echo "scale=1;$SUMT/10"|bc`
-## TODO : what todo withe the DATA
+sqlite3 ${DB} "insert into data (time,hum,temp) VALUES (${TIME},${HUMID},${TEMPERA});"
+#echo $HUMID
+#echo $TEMPERA
+cp $DB /var/www/
