b80b68e58276663231157210745ff50b5252e4e5
Urfin78 <urfin78@gmail.com>
2015-08-16 12:30:09 +0200

    several changes

--- graphrrd.sh
+++ graphrrd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #graph creation
-rrdtool graph /var/www/temperature.gif --start 1438424408 --end N --title="Temperatur"	-w 600 -h 200 --alt-autoscale-max --lower-limit 0 --vertical-label "Temperature" DEF:temp=temphum.rrd:TEMP:AVERAGE DEF:max=temphum.rrd:TEMP:MAX DEF:min=temphum.rrd:TEMP:MIN COMMENT:"Temperatur in °C \l" GPRINT:temp:MAX:"IN %6.2lf %s\:" GPRINT:temp:MIN:"%6.2lf %s\:" GPRINT:temp:AVERAGE:"%6.2lf %s\:" AREA:temp#00EE00:"Temp °C \l" LINE1:max#ff0000:"max" LINE2:min#0000ff:"min"
+RRD="/root/temphum.rrd"
+rrdtool graph /var/www/temperature.gif --start 1438424408 --end N --title="Temperatur"	-w 600 -h 200 --alt-autoscale-max --lower-limit 0 --vertical-label "Temperature" DEF:temp=${RRD}:TEMP:AVERAGE DEF:max=${RRD}:TEMP:MAX DEF:min=${RRD}:TEMP:MIN COMMENT:"Temperatur in °C \l" GPRINT:temp:MAX:"IN %6.2lf %s\:" GPRINT:temp:MIN:"%6.2lf %s\:" GPRINT:temp:AVERAGE:"%6.2lf %s\:" AREA:temp#00EE00:"Temp °C \l" LINE1:max#ff0000:"max" LINE2:min#0000ff:"min"
--- sql2rrd.sh
+++ sql2rrd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 DB="/root/test.db"
+RRD="/root/temphum.rrd"
 TIME=`date "+%s"`
-sqlite3 ${DB} "select time,temp,hum from data;"|sed 's/|/\:/g'|sed 's/^/rrdtool update temphum.rrd /' > 2rrd.sh
+echo '#!/bin/bash' > 2rrd.sh
+sqlite3 ${DB} "select time,temp,hum from data;"|sed 's/|/\:/g'|sed 's/^/rrdtool update \/root\/temphum.rrd /' >> 2rrd.sh
