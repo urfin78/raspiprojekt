7d55acbba1b3e3f099a7bba84af99f4273caa2af
Urfin78 <urfin78@gmail.com>
2015-08-16 12:31:10 +0200

    several changes

--- graphrrd.sh
+++ graphrrd.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 #graph creation
+START=1438424408
 RRD="/root/temphum.rrd"
-rrdtool graph /var/www/temperature.gif --start 1438424408 --end N --title="Temperatur"	-w 600 -h 200 --alt-autoscale-max --lower-limit 0 --vertical-label "Temperature" DEF:temp=${RRD}:TEMP:AVERAGE DEF:max=${RRD}:TEMP:MAX DEF:min=${RRD}:TEMP:MIN COMMENT:"Temperatur in °C \l" GPRINT:temp:MAX:"IN %6.2lf %s\:" GPRINT:temp:MIN:"%6.2lf %s\:" GPRINT:temp:AVERAGE:"%6.2lf %s\:" AREA:temp#00EE00:"Temp °C \l" LINE1:max#ff0000:"max" LINE2:min#0000ff:"min"
+MAX=`rrdtool graph ${RRD} --start=${START} DEF:max="${RRD}":TEMP:MAX VDEF:maxi=max,MAXIMUM PRINT:maxi:"%4.2lf %s"|tail -n 1`
+MIN=`rrdtool graph ${RRD} --start=${START} DEF:min="${RRD}":TEMP:MAX VDEF:mini=min,MINIMUM PRINT:mini:"%4.2lf %s"|tail -n 1`
+ULIMIT=`echo "scale=2;$MAX+5"|bc`
+LLIMIT=`echo "scale=2;$MIN-5"|bc`
+rrdtool graph /var/www/temperature.gif --start ${START} \
+	--title="Temperatur" \
+	-w 600 -h 200 \
+	--alt-autoscale-max \
+	--lower-limit ${LLIMIT} \
+	--upper-limit ${ULIMIT} \
+	--vertical-label "Temperature" \
+	DEF:temp=${RRD}:TEMP:AVERAGE \
+	DEF:max=${RRD}:TEMP:MAX \
+	DEF:min=${RRD}:TEMP:MIN \
+	COMMENT:"Temperatur in °C \l" \
+	LINE1:temp#00EE00:"Durchschnitt\:" \
+	GPRINT:temp:AVERAGE:"%6.2lf %s" \
+	LINE2:max#ff0000:"Max\:" \
+	GPRINT:max:MAX:"%6.2lf %s" \
+	LINE3:min#0000ff:"Min\:" \
+	GPRINT:min:MIN:"%6.2lf %s" \
