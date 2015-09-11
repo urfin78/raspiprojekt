0a592e95ffe21b514594a89be9c2d9c3f2052b66
Urfin78 <urfin78@gmail.com>
2015-09-11 18:10:59 +0200

    fixed and tested

--- repairsqlite.sh
+++ repairsqlite.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #variablen
-DB=
+DB="/root/test.db"
 WRONGTEMP=true
 #
 while [ ${WRONGTEMP} = true ]; do
-  TEMP=`sqlite3 ${DB} "select id from data where temp > 50 limit 1"`;
+  TEMP=`sqlite3 ${DB} 'select id from data where temp > 50 limit 1'`;
   if [ ${TEMP} ]; then
-    BEFORETEMP=`sqlite3 ${DB} "select temp from data where id='${TEMP}-1'"`;
-    AFTERTEMP=`sqlite3 ${DB} "select temp from data where id='${TEMP}+1'"`;
+    BEFORETEMP=`sqlite3 ${DB} 'select temp from data where id='${TEMP}-1''`;
+    AFTERTEMP=`sqlite3 ${DB} 'select temp from data where id='${TEMP}+1''`;
     NEWTEMP=`echo "scale=2;(${AFTERTEMP}+${BEFORETEMP})/2"|bc`;
-    sqlite3 ${DB} "update data set temp=${NEWTEMP} where id=${TEMP}`;
+    sqlite3 ${DB} "update data set temp=${NEWTEMP} where id=${TEMP}";
+#debugecho
+#echo "ID: ${TEMP} temp:${BEFORETEMP} temp2: ${AFTERTEMP} AVG:${NEWTEMP}";
   else
     WRONGTEMP=wrong;
+#debugecho
+#	echo "kein"
   fi
 done
