fbb998fe88de529b3bd7b1569036b55f043eb1db
Thomas Augustin <urfin78@gmail.com>
2016-07-11 11:50:19 +0200

    some changes for rrd creation

--- sql2rrd.sh
+++ sql2rrd.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-DB="/root/test.db"
-RRD="/root/temphum.rrd"
+#DB="/root/test.db"
+DB="test.db"
+#RRD="/root/temphum.rrd"
+RRD="newtemp.rrd"
 TIME=`date "+%s"`
-YESTERDAY="${TIME}-36000"
+#YESTERDAY="${TIME}-36000"
+YESTERDAY="${TIME}-1800"
 echo '#!/bin/bash' > 2rrd.sh
-sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update \/root\/temphum.rrd /' >> 2rrd.sh
+#sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update \/root\/temphum.rrd /' >> 2rrd.sh
+#sqlite3 ${DB} "select time,temp,hum from data;"|sed 's/|/\:/g'|sed 's/^/rrdtool update newtemp.rrd /' >> 2rrd.sh
+sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update newtemp.rrd /' >> 2rrd.sh
