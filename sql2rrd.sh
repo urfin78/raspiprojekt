9bd51d2688b83ec6d9f514beab3d5dfdde00d7e6
Thomas Augustin <urfin78@gmail.com>
2016-07-11 15:17:50 +0200

    added some test cases

--- sql2rrd.sh
+++ sql2rrd.sh
@@ -2,11 +2,11 @@
 #DB="/root/test.db"
 DB="test.db"
 #RRD="/root/temphum.rrd"
-RRD="newtemp.rrd"
+RRD="test.rrd"
 TIME=`date "+%s"`
 #YESTERDAY="${TIME}-36000"
 YESTERDAY="${TIME}-1800"
 echo '#!/bin/bash' > 2rrd.sh
 #sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update \/root\/temphum.rrd /' >> 2rrd.sh
-#sqlite3 ${DB} "select time,temp,hum from data;"|sed 's/|/\:/g'|sed 's/^/rrdtool update newtemp.rrd /' >> 2rrd.sh
-sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update newtemp.rrd /' >> 2rrd.sh
+sqlite3 ${DB} "select time,temp,hum from data;"|sed 's/|/\:/g'|sed 's/^/rrdtool update test.rrd /' >> 2rrd.sh
+#sqlite3 ${DB} "select time,temp,hum from data where time > ${YESTERDAY};"|sed 's/|/\:/g'|sed 's/^/rrdtool update newtemp.rrd /' >> 2rrd.sh
